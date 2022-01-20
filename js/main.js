87ba1c2149de9676a7a397618938d4fac0514b46
Elan van Biljon <e.vanbiljon@instadeep.com>
2022-01-20 17:46:02 +0200

    Update css and js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-/** 
+/**
  * ===================================================================
  * main js
  *
- * ------------------------------------------------------------------- 
- */ 
+ * -------------------------------------------------------------------
+ */
 
 (function($) {
 
@@ -11,16 +11,16 @@
 
 	/*---------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
    $(window).load(function() {
 
-      // will first fade out the loading animation 
+      // will first fade out the loading animation
     	$("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
         $("#preloader").delay(300).fadeOut("slow");
 
-      });       
+      });
 
   	})
 
@@ -37,13 +37,13 @@
 
 	/*---------------------------------------------------- */
 	/* FitVids
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
   	$(".fluid-video-wrapper").fitVids();
 
 
 	/*---------------------------------------------------- */
 	/* Owl Carousel
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
 	$("#owl-slider").owlCarousel({
         navigation: false,
         pagination: true,
@@ -61,7 +61,7 @@
   	------------------------------------------------------- */
 	$('.alert-box').on('click', '.close', function() {
 	  $(this).parent().fadeOut(500);
-	});	
+	});
 
 
 	/*----------------------------------------------------- */
@@ -74,7 +74,7 @@
 
    	handler: function(direction) {
 
-      	if (direction === "down") {       		
+      	if (direction === "down") {
 
 			   stats.each(function () {
 				   var $this = $(this);
@@ -88,16 +88,16 @@
 				  	});
 				});
 
-       	} 
+       	}
 
        	// trigger once only
-       	this.destroy();      	
+       	this.destroy();
 
 		},
-			
+
 		offset: "90%"
-	
-	});	
+
+	});
 
 
 	/*---------------------------------------------------- */
@@ -107,9 +107,9 @@
 
 	containerProjects.imagesLoaded( function() {
 
-		containerProjects.masonry( {		  
+		containerProjects.masonry( {
 		  	itemSelector: '.folio-item',
-		  	resize: true 
+		  	resize: true
 		});
 
 	});
@@ -133,30 +133,74 @@
    	$.magnificPopup.close();
    });
 
-	
+	var tooltips = document.querySelectorAll('.tooltip span.tooltiptext');
+	window.onmousemove = function (e) {
+			var x = (e.clientX + 20) + 'px',
+					y = (e.clientY - 15) + 'px';
+					// y = (e.clientY - 20) + 'px';
+			for (var i = 0; i < tooltips.length; i++) {
+					tooltips[i].style.top = y;
+					tooltips[i].style.left = x;
+			}
+	};
+
 	/*-----------------------------------------------------*/
   	/* Navigation Menu
-   ------------------------------------------------------ */  
+   ------------------------------------------------------ */
    var toggleButton = $('.menu-toggle'),
        nav = $('.main-navigation');
+		var cvDownloadButton = $('.relative-button');
+		var down = false;
+		// var distance = nav.height();
+		var distance = nav.outerHeight();
+		// console.log(`The hight of the nav is ${distance}.`)
 
    // toggle button
    toggleButton.on('click', function(e) {
 
 		e.preventDefault();
 		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
+		// nav.slideToggle({
+		// 	duration: 100,
+		// 	easing: "linear"
+		// });
 
+		// get height position of nav
+		// get height of nav
+		// move download button (change hight position) in slide
+		// cvDownloadButton.toggleClass('is-clicked');
+		// console.log(`Moving download button by ${distance}.`)
+
+		if (down) {
+			cvDownloadButton.css({'transform': `translateY(0px)`});
+			nav.slideToggle({
+				duration: 300,
+				easing: "linear"
+			});
+			// cvDownloadButton.css({'transform': 'translateY(0px)'});
+		} else {
+			cvDownloadButton.css({'transform': `translateY(${distance}px)`});
+			nav.slideToggle({
+				duration: 100,
+				easing: "linear"
+			});
+			// cvDownloadButton.css({'transform': 'translateY(100px)'});
+		}
+		down = !down;
 	});
 
    // nav items
-  	nav.find('li a').on("click", function() {   
+  	nav.find('li a').on("click", function() {
 
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
+   	// update the toggle button
+   	toggleButton.toggleClass('is-clicked');
+			cvDownloadButton.css({'transform': `translateY(0px)`});
+			down = !down;
+			// cvDownloadButton.css({'transform': 'translateY(-100px)'});
+		// cvDownloadButton.toggleClass('is-clicked');
+		// fadeout the navigation panel
+   	nav.fadeOut();
+
   	});
 
 
@@ -164,7 +208,7 @@
   	/* Highlight the current section in the navigation bar
   	------------------------------------------------------ */
 	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
+	navigation_links = $("#main-nav-wrap li a");
 
 	sections.waypoint( {
 
@@ -176,12 +220,12 @@
 
 			if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
 
-		}, 
+		},
 
 		offset: '25%'
 	});
@@ -191,7 +235,7 @@
   	/* Smooth Scrolling
   	------------------------------------------------------ */
   	$('.smoothscroll').on('click', function (e) {
-	 	
+
 	 	e.preventDefault();
 
    	var target = this.hash,
@@ -203,13 +247,13 @@
       	window.location.hash = target;
       });
 
-  	});  
-  
+  	});
+
 
    /*---------------------------------------------------- */
 	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */ 
-	$('input, textarea, select').placeholder()  
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()
 
 
   	/*---------------------------------------------------- */
@@ -224,28 +268,28 @@
 
 			var sLoader = $('#submit-loader');
 
-			$.ajax({      	
+			$.ajax({
 
 		      type: "POST",
 		      url: "inc/sendEmail.php",
 		      data: $(form).serialize(),
-		      beforeSend: function() { 
+		      beforeSend: function() {
 
-		      	sLoader.fadeIn(); 
+		      	sLoader.fadeIn();
 
 		      },
 		      success: function(msg) {
 
 	            // Message was sent
 	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
+	            	sLoader.fadeOut();
 	               $('#message-warning').hide();
 	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
+	               $('#message-success').fadeIn();
 	            }
 	            // There was an error
 	            else {
-	            	sLoader.fadeOut(); 
+	            	sLoader.fadeOut();
 	               $('#message-warning').html(msg);
 		            $('#message-warning').fadeIn();
 	            }
@@ -253,13 +297,13 @@
 		      },
 		      error: function() {
 
-		      	sLoader.fadeOut(); 
+		      	sLoader.fadeOut();
 		      	$('#message-warning').html("Something went wrong. Please try again.");
 		         $('#message-warning').fadeIn();
 
 		      }
 
-	      });     		
+	      });
   		}
 
 	});
@@ -267,7 +311,7 @@
 
  	/*----------------------------------------------------- */
   	/* Back to top
-   ------------------------------------------------------- */ 
+   ------------------------------------------------------- */
 	var pxShow = 300; // height on which the button will show
 	var fadeInTime = 400; // how slow/fast you want the button to show
 	var fadeOutTime = 400; // how slow/fast you want the button to hide
@@ -284,8 +328,8 @@
 				jQuery("#go-top").fadeOut(fadeOutTime);
 			}
 
-		}		
+		}
 
-	});		
+	});
 
 })(jQuery);
