caf1bc8716b7a3a147c94a1c6d1f6e07caf5153e
Elan van Biljon <e.vanbiljon@instadeep.com>
2022-01-22 15:19:28 +0200

    Update modal: remove tooltips, add open modal button

--- js/main.js
+++ js/main.js
@@ -141,16 +141,16 @@
    	$.magnificPopup.close();
    });
 
-	var tooltips = document.querySelectorAll('.tooltip span.tooltiptext');
-	window.onmousemove = function (e) {
-			var x = (e.clientX + 20) + 'px',
-					y = (e.clientY - 15) + 'px';
-					// y = (e.clientY - 20) + 'px';
-			for (var i = 0; i < tooltips.length; i++) {
-					tooltips[i].style.top = y;
-					tooltips[i].style.left = x;
-			}
-	};
+	// var tooltips = document.querySelectorAll('.tooltip span.tooltiptext');
+	// window.onmousemove = function (e) {
+	// 		var x = (e.clientX + 20) + 'px',
+	// 				y = (e.clientY - 15) + 'px';
+	// 				// y = (e.clientY - 20) + 'px';
+	// 		for (var i = 0; i < tooltips.length; i++) {
+	// 				tooltips[i].style.top = y;
+	// 				tooltips[i].style.left = x;
+	// 		}
+	// };
 
 	/*-----------------------------------------------------*/
   	/* Navigation Menu
